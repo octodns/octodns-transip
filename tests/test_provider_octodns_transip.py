a535c10a867ba7af9e84fcc399ab93c0467387d4
Ross McFarland <rwmcfa1@gmail.com>
2022-01-14 14:06:18 -0800

    Import skeleton

---
+++ tests/test_provider_octodns_transip.py
@@ -0,0 +1,15 @@
+#
+#
+#
+
+from unittest import TestCase
+
+from octodns_transip import TransipProvider
+
+
+class TestTransipProvider(TestCase):
+
+    # TODO: test provider
+    def test_nothing(self):
+        self.assertTrue(True)
+        TransipProvider
