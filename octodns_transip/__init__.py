fc5d191e4b69514ad20f896ad6427646f0bb9023
Maikel Poot <maikel.poot@topicus.nl>
2024-10-11 07:52:51 +0200

    Revert "Apply suggestions from code review"
    This reverts commit 62f93de846c14496f98096393832fb5506fd9b09.

--- octodns_transip/__init__.py
+++ octodns_transip/__init__.py
@@ -214,7 +214,7 @@ class TransipProvider(BaseProvider):
             if record._type == 'NS' and record.name == '':
 
                 # Check values for FQDN, IP's are not supported
-                values = record.values
+                values = record.values if record.values else [record.value]
 
                 for value in values:
 
@@ -252,7 +252,11 @@ class TransipProvider(BaseProvider):
             record = change.data['new']
 
             if change.data['name'] == '' and change.data['record_type'] == 'NS':
-                values = record.values
+                values = (
+                    record.get('values')
+                    if record.get('values')
+                    else [record.get('value')]
+                )
 
                 nameservers = []
                 for value in values:
